3838a5078af75217ba6babaa87e28a5de1d79767
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2012-08-20 11:51:05 +0000

    trunk/sc/ Adding 'vartab_insert_modules_name()' to the vartab.h header file.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/compilers/trunk@2287 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/vartab.h
+++ sc/vartab.h
@@ -28,6 +28,9 @@ void vartab_insert_named( VARTAB *table, DNODE *dnode, cexception_t *ex );
 void vartab_insert( VARTAB *table, const char *name,
 		    DNODE *tnode, cexception_t *ex );
 
+void vartab_insert_modules_name( VARTAB *table, const char *name,
+                                 DNODE *dnode, cexception_t *ex );
+
 void vartab_copy_table( VARTAB *dst, VARTAB *src, cexception_t *ex );
 
 DNODE *vartab_lookup( VARTAB *table, const char *name );
