0568fdcb8b3d50a23345633ea5819009859fb980
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2013-02-03 10:46:31 +0000

    branches/packed-type-representation/sc/ Preparing the garbage collector traverser to travese the new memory layout; so far all allocated types occupy the whole stackcell; but when the layout will change, alternative code will have to be used (that code is now commented out).
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/compilers/branches/packed-type-representation@2408 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/bctraverse.c
+++ sc/bctraverse.c
@@ -48,8 +48,9 @@ end
 void bctraverse( void *root )
 {
     alloccell_t *p, *q, *r;
-    stackcell_t *s;
-    int k;
+    void **base, **e;
+    // void *s;
+    ssize_t k, i;
     int debug = thrcode_heapdebug_is_on();
 
     if( !root ) return;
@@ -58,30 +59,60 @@ void bctraverse( void *root )
     for(;;) {
 	assert( p != NULL );
 	assert( p[-1].magic == BC_MAGIC );
+        if( p[-1].nref > 0  ) {
+            if( p[-1].length > 0 &&
+                p[-1].element_size == sizeof(stackcell_t) ) {
+                base = (void**)&(((stackcell_t*)p)->ptr);
+            } else {
+                // base = (void**)p;
+                base = (void**)&(((stackcell_t*)p)->ptr);
+            }
+        } else {
+            base = (void**)(&p[-1]) - 1;
+        }
 	k = p[-1].rcount;
-	s = (stackcell_t*)p + k;
-	if( debug ) {
+        i = p[-1].nref >= 0 ? k : -k;
+        e = i > 0 ? 
+            (void**)(((char*)base) + 
+                     /* p[-1].element_size */sizeof(stackcell_t) * i) :
+            base + i;
+        if( debug ) {
 	    printf( "%10p (%10p) (up: %10p) nref = %4d length = %8d, k = %d\n",
 		    p-1, p, q, p[-1].nref, p[-1].length, k );
 	}
 	p[-1].flags |= AF_USED;
 	p[-1].rcount ++;
-	if( k < p[-1].nref ) {
-	    r = s->ptr;
+	if( k < abs( p[-1].nref )) {
+	    r = *e;
 	    if( r != NULL && 
                 ( (char*)r <  (char*)istate.code ||
 		  (char*)r >= (char*)(istate.code + istate.code_length) ) &&
                 (r[-1].flags & AF_USED) == 0 ) {
-		s->ptr = q;
+		*e = q;
 		q = p;
 		p = r;
 	    }
 	} else if( p == q ) {
 	    break;
 	} else {
+            if( q[-1].nref > 0  ) {
+                if( q[-1].length > 0 &&
+                    q[-1].element_size == sizeof(stackcell_t) ) {
+                    base = (void**)&(((stackcell_t*)q)->ptr);
+                } else {
+                    // base = (void**)q;
+                    base = (void**)&(((stackcell_t*)q)->ptr);
+                }
+            } else {
+                base = (void**)(&q[-1]) - 1;
+            }
 	    k = q[-1].rcount - 1;
-            s = (stackcell_t*)q + k;
-	    r = s->ptr; s->ptr = p;
+            i = q[-1].nref > 0 ? k : -k;
+            e = i > 0 ?
+                (void**)(((char*)base) + 
+                         /* p[-1].element_size */sizeof(stackcell_t) * i) :
+                base + i;
+	    r = *e; *e = p;
 	    p = q; q = r;
 	}
     }
