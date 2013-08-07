3ed1ad78a09e035b0a37cd7d7f4a84aab53d26fd
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2013-08-07 12:05:27 +0000

    branches/packed-type-representation/ Fixing implementation of hashes in the new packed type representation. The old hash001 test is now passing again, and the code should be compatible with the mamery layout and teh garbage collector.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/packed-type-representation@2464 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/bcalloc.h
+++ sc/bcalloc.h
@@ -25,6 +25,8 @@ typedef enum {
 void bcalloc_set_gc_collector_policy( gc_policy_t new_policy );
 gc_policy_t bcalloc_gc_collector_policy( void );
 void bc_merror( cexception_t *ex );
+void *bcalloc_memory( size_t size, short element_size,
+                      ssize_t length, ssize_t nref );
 void *bcalloc( size_t size, ssize_t nref );
 void *bcalloc_blob( size_t size );
 char* bcstrdup( char *str );
