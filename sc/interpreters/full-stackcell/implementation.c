d670c24cf7cc3dadb20c186b765bed3040c55fd2
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2014-05-24 16:20:40 +0000

    branches/all-representations/sc/ (saulius@kolibris) Collecting files for full stackcell representation, and patching them so that the 'make -k INTERPRETER_DIR=interpreters/full-stackcell' command compiles the main compiler and interpreter. Still, the libraries can not be compiled, and many tests fail; one of the problems is incompatible 'bcalloc_array()' function.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/all-representations@2606 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

---
+++ sc/interpreters/full-stackcell/implementation.c
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------*\
+**$Author$
+**$Date$ 
+**$Revision$
+**$URL$
+\*---------------------------------------------------------------------------*/
+
+/* exports: */
+#include <implementation.h>
+
+/* uses: */
+#include <string.h>
+
+int implementation_has_attribute( char *attribute )
+{
+    if( strcmp( attribute, "element_size" ) == 0 ) {
+        return 0;
+    }
+    return 1;
+}
+
+int implementation_has_format( char format )
+{
+    if( format == 's' ) return 0;
+    return 1;
+}
