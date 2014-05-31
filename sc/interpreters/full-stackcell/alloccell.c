efe142997d0555cdbc8f36029d31116e5c2a5b84
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2014-05-31 16:00:25 +0000

    branches/all-representations/ (saulius@kolibris) Makeing an interpretation-specific function alloccell_set_values() that handles correct alloccell_t structure initialisation for different implementations where this structure may have different fields and even different values in these fields. Now all tests using the packed-type/ representation pass, and the full-stackcell/ representation can be compiled (although many tests still fail).
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/all-representations@2611 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

---
+++ sc/interpreters/full-stackcell/alloccell.c
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------*\
+**$Author$
+**$Date$ 
+**$Revision$
+**$URL$
+\*---------------------------------------------------------------------------*/
+
+/* exports: */
+#include <alloccell.h>
+
+/* uses: */
+#include <assert.h>
+
+void alloccell_set_values( alloccell_t *hdr, ssize_t element_size,
+                           ssize_t len )
+{
+    assert( hdr );
+    hdr->magic = BC_MAGIC;
+    hdr->flags |= AF_USED;
+    hdr->length = len;
+    /* hdr->element_size = element_size; */
+}
+
