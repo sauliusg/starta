71e070c7829910132d450428f588da20258167c1
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2014-06-02 16:28:55 +0000

    branches/all-representations/ (saulius@kolibris) Parametrising compilation of libraries/arraymath/, to enable compilation of the array-processing functions for different implementations from the same source.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/all-representations@2613 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

---
+++ sc/interpreters/packed-type/arrays.h
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------*\
+**$Author$
+**$Date$ 
+**$Revision$
+**$URL$
+\*---------------------------------------------------------------------------*/
+
+/* Macros for different array implementations in the runtime system of
+   the compiler: */
+
+#ifndef __ARRAYS_H
+#define __ARRAYS_H
+
+#define ARRAY_ELEMENT(a) (a)
+
+#endif
