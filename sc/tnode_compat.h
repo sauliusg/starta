67e364e47059dedaa54f44435904b2c706a13544
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2014-06-12 16:23:49 +0000

    branches/all-representations/ (saulius@kolibris) Moving the type compatibility code to 'tnode_compat.[ch]', to be shared between implemenations.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/all-representations@2626 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

---
+++ sc/tnode_compat.h
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------*\
+**$Author$
+**$Date$ 
+**$Revision$
+**$URL$
+\*---------------------------------------------------------------------------*/
+
+#ifndef __TNODE_COMPAT_H
+#define __TNODE_COMPAT_H
+
+#include <tnode.h>
+#include <typetab.h>
+#include <cexceptions.h>
+
+int tnode_types_are_compatible( TNODE *t1, TNODE *t2,
+				TYPETAB *generic_types,
+				cexception_t *ex );
+
+int tnode_types_are_assignment_compatible( TNODE *t1, TNODE *t2,
+                                           TYPETAB *generic_types,
+                                           cexception_t *ex );
+
+int tnode_types_are_identical( TNODE *t1, TNODE *t2,
+			       TYPETAB *generic_types,
+			       cexception_t *ex );
+
+#endif
