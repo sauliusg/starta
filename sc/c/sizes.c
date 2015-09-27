2a68597f98816600a42a3776c7179e576228438c
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2015-09-27 13:42:26 +0000

    Updating C tests for checking type sizes.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/saulius-parmetrised-modules@3232 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/c/sizes.c
+++ sc/c/sizes.c
@@ -3,9 +3,15 @@
 
 int main()
 {
-    printf( "sizeof(intmax_t) = %jd\n", (intmax_t)sizeof(intmax_t) );
-    printf( "sizeof(ssize_t)  = %jd\n", (intmax_t)sizeof(ssize_t) );
-    printf( "sizeof(void*)    = %jd\n", (intmax_t)sizeof(void*) );
-    printf( "sizeof(int)      = %jd\n", (intmax_t)sizeof(int) );
+    printf( "sizeof(intmax_t)  = %jd\n", (intmax_t)sizeof(intmax_t) );
+    printf( "sizeof(ssize_t)   = %jd\n", (intmax_t)sizeof(ssize_t) );
+    printf( "sizeof(void*)     = %jd\n", (intmax_t)sizeof(void*) );
+    printf( "sizeof(int)       = %jd\n", (intmax_t)sizeof(int) );
+    printf( "sizeof(long long) = %jd\n", (intmax_t)sizeof(long long) );
+
+    long long i = 1000000000000LL;
+
+    printf( "long long i = %jd\n", (intmax_t)i );
+
     return 0;
 }
