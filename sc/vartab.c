a7e159016dce4a0f0376b2625e69b5a6450ff31b
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2015-12-04 13:49:21 +0000

    Large refactoring: now using word 'module' consistently throughout the source code. The word 'package' is no longer used as a synonim of 'module', but is reserved for future use (possible 'packages' will be collections of modules). The keyword 'package 'is however still reserved and synonimous to the keyword 'module', to maintain compatibility with the old versions.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/trunk@3332 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/vartab.c
+++ sc/vartab.c
@@ -303,7 +303,7 @@ DNODE *vartab_lookup( VARTAB *table, const char *name )
     VAR_NODE *node = vartab_lookup_varnode( table, name );
     if( node && node->count > 1 ) {
         yyerrorf( "name '%s' is imported more than once -- "
-                  "please use explicit package name for disambiguation",
+                  "please use explicit module name for disambiguation",
                   name );
     }
     return node ? node->dnode : NULL;
