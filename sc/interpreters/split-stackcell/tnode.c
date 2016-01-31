1f9aa7c0b4afe03d8e910d1abc1c2a8501142451
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2016-01-31 12:41:39 +0000

    branches/saulius-destructors/ (saulius@kolibris) Merging the recent trunk/ code into the saulius-destructors/ branch.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/branches/saulius-destructors@3457 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/interpreters/split-stackcell/tnode.c
+++ sc/interpreters/split-stackcell/tnode.c
@@ -163,9 +163,9 @@ TNODE *new_tnode_forward_struct( char *name, cexception_t *ex )
     cexception_t inner;
     TNODE * volatile node = new_tnode( ex );
 
-    assert( name );
     cexception_guard( inner ) {
-	node->name = strdupx( name, &inner );
+        if( name )
+            node->name = strdupx( name, &inner );
 	node->kind = TK_STRUCT;
 	tnode_set_flags( node, TF_IS_FORWARD | TF_IS_REF );
     }
@@ -181,9 +181,9 @@ TNODE *new_tnode_forward_class( char *name, cexception_t *ex )
     cexception_t inner;
     TNODE * volatile node = new_tnode( ex );
 
-    assert( name );
     cexception_guard( inner ) {
-	node->name = strdupx( name, &inner );
+        if( name )
+            node->name = strdupx( name, &inner );
 	node->kind = TK_CLASS;
 	tnode_set_flags( node, TF_IS_FORWARD | TF_IS_REF );
     }
@@ -1067,6 +1067,21 @@ ssize_t tnode_max_interface( TNODE *class_descr )
     }
 
     return max_interface;
+}
+
+ssize_t tnode_base_class_count( TNODE *tnode )
+{
+    ssize_t count = 0;
+
+    if( !tnode )
+        return 0;
+
+    while( tnode->base_type && tnode->base_type->kind == TK_CLASS ) {
+        count ++;
+        tnode = tnode->base_type;
+    }
+
+    return count;
 }
 
 /* The full stackcell implementation does not need to align fields --
