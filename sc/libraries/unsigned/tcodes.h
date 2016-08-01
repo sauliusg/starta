e28edd0261401f140e5ee14e6e41e4ab2b89c394
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2016-08-01 12:25:01 +0000

    Preparing the .so library for the unsigned arithmentic module.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/trunk@3633 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

---
+++ sc/libraries/unsigned/tcodes.h
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------*\
+**$Author$
+**$Date$ 
+**$Revision$
+**$URL$
+\*---------------------------------------------------------------------------*/
+
+#ifndef __TCODES_H
+#define __TCODES_H
+
+#include <run.h>
+
+#if 0
+#define INSTRUCTION_FN_ARGS instruction_t *code, cexception_t *ex
+#else
+#define INSTRUCTION_FN_ARGS void
+#endif
+
+#include "locally-generated/arithm_uint.h"
+#include "locally-generated/unsigned_uint.h"
+
+#endif
