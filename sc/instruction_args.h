bb50cff110b99f46b28ef18ce6dd6c66370c387d
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2017-05-15 11:44:07 +0000

    trunk/ (saulius@koala.ibt.lt) Adding a module to check whether a string conforms to number syntax.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/compilers/trunk@3827 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

---
+++ sc/instruction_args.h
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------*\
+**$Author$
+**$Date$ 
+**$Revision$
+**$URL$
+\*---------------------------------------------------------------------------*/
+
+#ifndef __INSTRUCTION_ARGS_H
+#define __INSTRUCTION_ARGS_H
+
+#if 0
+#define INSTRUCTION_FN_ARGS instruction_t *code, cexception_t *ex
+#else
+#define INSTRUCTION_FN_ARGS void
+#endif
+
+#endif
