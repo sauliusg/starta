bedd8da1cc6d17e0df00602877b1bfab21333740
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2017-11-06 19:55:36 +0000

    trunk/sc/libraries/ (saulius@varanas.saulius-grazulis.lt) Includeing 'ctype.h' into unsigned/opcodes.c, eliminating one (but not all) undeclared funciton warnings.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/compilers/trunk@3871 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- sc/libraries/unsigned/opcodes.c
+++ sc/libraries/unsigned/opcodes.c
@@ -11,7 +11,9 @@
 /* uses: */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <limits.h> /* for the ..._MAX constants */
 #include <errno.h>
 #include <assert.h>
