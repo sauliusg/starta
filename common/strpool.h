a30354fbb5ee5c6b2f22e516e22132e641adfec0
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2018-03-26 17:21:21 +0000

    branches/saulius-fight-memory-leaks/ (saulius@varanas.saulius-grazulis.lt) Changing string pool implementation to use STRPOOL object instead of static pool array. In this way, string pools are fore sure reenterable and thread safe.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/compilers/branches/saulius-fight-memory-leaks@3986 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- common/strpool.h
+++ common/strpool.h
@@ -11,17 +11,18 @@
 #include <stdlib.h>
 #include <cexceptions.h>
 
-typedef union {
-    int next;
-    char *str;
-} strpool_t;
+typedef struct STRPOOL STRPOOL;
 
-ssize_t pool_insert_string( char *volatile *str, cexception_t *ex );
-ssize_t pool_add_string( char *str, cexception_t *ex );
-ssize_t pool_clone_string( char *str );
-ssize_t pool_strnclone( char *str, size_t length );
-char *obtain_string_from_pool( ssize_t index );
-char *get_string_from_pool( ssize_t index );
-void free_pool();
+STRPOOL *new_strpool( cexception_t *ex );
+void delete_strpool( STRPOOL *pool );
+void dispose_strpool( STRPOOL *volatile *pool );
+void free_strpool( STRPOOL *p );
+
+ssize_t pool_insert_string( STRPOOL *p, char *volatile *str, cexception_t *ex );
+ssize_t pool_add_string( STRPOOL *p, char *str, cexception_t *ex );
+ssize_t pool_clone_string( STRPOOL *p, char *str );
+ssize_t pool_strnclone( STRPOOL *p, char *str, size_t length );
+char *obtain_string_from_pool( STRPOOL *p, ssize_t index );
+char *get_string_from_pool( STRPOOL *p, ssize_t index );
 
 #endif
