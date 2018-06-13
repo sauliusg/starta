7efb07b232bf850fa9aa6fefaf78fe766775359a
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2018-06-13 06:22:07 +0000

    Fixing memory management after destructor runs.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/compilers/branches/saulius-fight-memory-leaks@4131 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- starta/interpreters/packed-type/bcalloc.c
+++ starta/interpreters/packed-type/bcalloc.c
@@ -311,18 +311,28 @@ void bcalloc_run_all_destructors( cexception_t *ex )
     alloccell_t *curr;
     alloccell_t *finalised;
     int i = 0;
-    //do {
-    finalised = allocated; //allocated = NULL;
+    int finished = 0;
+    do {
+        finalised = allocated; allocated = NULL;
         for( curr = finalised; curr != NULL; curr = curr->next ) {
             thrcode_run_destructor_if_needed( &istate, curr, ex );
         }
-        //if( ++i >= 1000 ) {
-        //    fprintf( stderr, "%d destructor cycles did not eliminte "
-        //             "unallocated objects -- bailing out...\n", i );
-        //    fflush( NULL );
-        //    exit( 98 );
-        //}
-        //} while( allocated );
+        if( ++i >= 1000 ) {
+            fprintf( stderr, "%d destructor cycles did not eliminte "
+                     "unallocated objects -- bailing out...\n", i );
+            fflush( NULL );
+            exit( 98 );
+        }
+        /* Append the finalised objects to the newly allocated
+           block list: */
+        if( allocated ) {
+            for( curr = allocated; curr->next != NULL; curr = curr->next );
+            curr->next = finalised;
+        } else {
+            allocated = finalised;
+            finished = 1;
+        }
+    } while( !finished );
 }
 
 int bcalloc_is_in_heap( void *p )
