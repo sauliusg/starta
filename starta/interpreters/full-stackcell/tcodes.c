1c17227fceca5437d82e7bf48a882abb839a57e0
saulius <saulius@f7c8cbe2-f9e6-0310-9050-d7f47bf15973>
2025-10-11 08:55:22 +0000

    Allowing to push onto empty arrays. This fixes two tests.
    branches/saulius-type-system-redesign/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/compilers/branches/saulius-type-system-redesign@5054 f7c8cbe2-f9e6-0310-9050-d7f47bf15973

--- starta/interpreters/full-stackcell/tcodes.c
+++ starta/interpreters/full-stackcell/tcodes.c
@@ -901,13 +901,8 @@ int APUSH( INSTRUCTION_FN_ARGS )
     TRACE_FUNCTION();
 
     if( !array ) {
-	interpret_raise_exception_with_bcalloc_message
-	    ( /* err_code = */ -1,
-	      /* message = */
-	      "can not push value onto a null array",
-	      /* module_id = */ 0,
-	      /* exception_id = */ SL_EXCEPTION_ARRAY_OVERFLOW,
-	      EXCEPTION );
+        /* Just return the pushed 'value' array: */
+        istate.ep[1] = istate.ep[0];
     } else {
         flags = array[-1].flags;
         nref = array[-1].nref;
